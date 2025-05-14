e817fb0d1f781f18d6d35d632498b547c680c670
minseo <yeominseo59618@gmail.com>
2025-05-15 00:45:30 +0900

    feat: top-bar blur 적용

--- src/app/(pages)/notice/page.tsx
+++ src/app/(pages)/notice/page.tsx
@@ -1,5 +1,9 @@
 import TopBar from '@/app/_common/components/top-bar';
 
 export default function Notice() {
-  return <TopBar title='공지사항' />;
+  return (
+    <main>
+      <TopBar title='공지사항' bgClassName='bg-white/20 backdrop-blur-md p-4' />
+    </main>
+  );
 }
