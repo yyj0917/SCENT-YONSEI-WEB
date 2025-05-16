49ae7833c35444cc1e0b32315ae28b45b5347866
LeeYonDu <gj5051129@gmail.com>
2025-05-16 17:12:02 +0900

    feat: makers insta link, review page connect

---
+++ src/app/(pages)/review/layout.tsx
@@ -0,0 +1,11 @@
+import UnderBar from '../makers/_components/underbar';
+import ReviewPage from './page';
+
+export default function MakersLayout() {
+  return (
+    <div>
+      <ReviewPage />
+      <UnderBar />
+    </div>
+  );
+}
