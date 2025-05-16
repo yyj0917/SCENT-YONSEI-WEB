0e55bc22efb7934f3e8b8eebd08194311fabb207
minseo <yeominseo59618@gmail.com>
2025-05-16 23:03:23 +0900

    feat: campusfilter button 생성 및 스타일링

---
+++ src/app/(pages)/notice/_components/CampusFilter.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from 'react';
+
+const campuses = ['블루런', '국제캠', '신촌캠'];
+
+export default function CampusFilter() {
+  const [selected, setSelected] = useState('블루런');
+  return (
+    <div className='flex gap-[12px] mt-[150px] ml-[25.5px] w-[324px] h-[44px]'>
+      {campuses.map(campus => (
+        <button
+          key={campus}
+          onClick={() => setSelected(campus)}
+          className={`w-[100px] rounded-[100px] px-[16px] pt-[10px] pb-[10px] font-semibold transition
+            ${
+              selected === campus
+                ? 'bg-[rgba(7,98,173,1)] text-[rgba(255,255,255,1)]'
+                : 'bg-[rgba(223,244,255,1)] text-[rgba(25,29,31,1)]'
+            }`}
+        >
+          {campus}
+        </button>
+      ))}
+    </div>
+  );
+}
--- src/app/(pages)/notice/page.tsx
+++ src/app/(pages)/notice/page.tsx
@@ -1,9 +1,13 @@
 import TopBar from '@/app/_common/components/top-bar';
+import CampusFilter from './_components/CampusFilter';
 
 export default function Notice() {
   return (
     <main>
       <TopBar title='공지사항' bgClassName='bg-white/20 backdrop-blur-md p-4' />
+      <section className='p-4'>
+        <CampusFilter />
+      </section>
     </main>
   );
 }
