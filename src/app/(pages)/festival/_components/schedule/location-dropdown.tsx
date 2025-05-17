cc04fd7100dbcab77ed4fd2f85451daae3113719
eric-jy-park <2019147551@yonsei.ac.kr>
2025-05-17 18:12:20 +0900

    fix: fix hr styles on dropdown animation

--- src/app/(pages)/festival/_components/schedule/location-dropdown.tsx
+++ src/app/(pages)/festival/_components/schedule/location-dropdown.tsx
@@ -58,62 +58,69 @@ export const LocationDropdown = () => {
           <ChevronDownIcon className='size-8' strokeWidth={1.5} />
         </motion.div>
       </button>
-      <hr className='w-full border-gray300 absolute top-13' />
 
       <AnimatePresence initial={false}>
         {isOpen && (
-          <motion.div
-            initial={{ height: 0, opacity: 0 }}
-            animate={{
-              height: 'calc(100% - 36px)',
-              opacity: 1,
-              transition: {
-                height: {
-                  duration: 0.3,
-                  ease: [0.645, 0.045, 0.355, 1],
+          <>
+            <motion.hr
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.2 } }}
+              exit={{ opacity: 0 }}
+              className='w-full border-gray300 absolute top-13'
+            />
+            <motion.div
+              initial={{ height: 0, opacity: 0 }}
+              animate={{
+                height: 'calc(100% - 36px)',
+                opacity: 1,
+                transition: {
+                  height: {
+                    duration: 0.3,
+                    ease: [0.645, 0.045, 0.355, 1],
+                  },
+                  opacity: {
+                    duration: 0.2,
+                    delay: 0.1,
+                    ease: [0.645, 0.045, 0.355, 1],
+                  },
                 },
-                opacity: {
-                  duration: 0.2,
-                  delay: 0.1,
-                  ease: [0.645, 0.045, 0.355, 1],
+              }}
+              exit={{
+                height: 0,
+                opacity: 0,
+                transition: {
+                  height: {
+                    duration: 0.3,
+                    ease: [0.645, 0.045, 0.355, 1],
+                  },
+                  opacity: {
+                    duration: 0.2,
+                    delay: 0.1,
+                    ease: [0.645, 0.045, 0.355, 1],
+                  },
                 },
-              },
-            }}
-            exit={{
-              height: 0,
-              opacity: 0,
-              transition: {
-                height: {
-                  duration: 0.3,
-                  ease: [0.645, 0.045, 0.355, 1],
-                },
-                opacity: {
-                  duration: 0.2,
-                  delay: 0.1,
-                  ease: [0.645, 0.045, 0.355, 1],
-                },
-              },
-            }}
-            className='bg-white -mt-5 pt-9 overflow-hidden border-1 border-light200 rounded-b-[20px] border-t-0 p-4'
-          >
-            <div className='w-full flex flex-col items-center justify-center gap-5'>
-              <div className='flex gap-6 w-full items-center justify-center'>
-                {locationList.map(location => (
-                  <button
-                    key={location.value}
-                    onClick={() => handleLocationChange(location.value)}
-                    className={cn(
-                      'text-headline-l font-semibold text-gray500 transition-colors duration-300',
-                      location.value === currentLocation && 'text-point',
-                    )}
-                  >
-                    {location.label}
-                  </button>
-                ))}
+              }}
+              className='bg-white -mt-5 pt-9 overflow-hidden border-1 border-light200 rounded-b-[20px] border-t-0 p-4'
+            >
+              <div className='w-full flex flex-col items-center justify-center gap-5'>
+                <div className='flex gap-6 w-full items-center justify-center'>
+                  {locationList.map(location => (
+                    <button
+                      key={location.value}
+                      onClick={() => handleLocationChange(location.value)}
+                      className={cn(
+                        'text-headline-l font-semibold text-gray500 transition-colors duration-300',
+                        location.value === currentLocation && 'text-point',
+                      )}
+                    >
+                      {location.label}
+                    </button>
+                  ))}
+                </div>
+                <div className='rounded-[10px] bg-gray-300 w-full h-[215px]' />
               </div>
-              <div className='rounded-[10px] bg-gray-300 w-full h-[215px]' />
-            </div>
-          </motion.div>
+            </motion.div>
+          </>
         )}
       </AnimatePresence>
     </div>
