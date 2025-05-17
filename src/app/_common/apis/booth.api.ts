cb09b637c43594a7ded1ba80c065593ed985c661
yyj0917 <yyj0917@yonsei.ac.kr>
2025-05-17 18:01:45 +0900

    feat: tanstack query settings... server component & client component

---
+++ src/app/_common/apis/booth.api.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import axios from 'axios';
+import { Booth, BoothDetail } from '../interfaces/booth.interface';
+import { axiosInstance } from '../utils/axios-instance';
+import { Section } from '@/app/(pages)/booth/types/booth-union.type';
+import { Category } from '@/app/(pages)/booth/types/booth-union.type';
+import { Day } from '@/app/(pages)/booth/types/booth-union.type';
+import {
+  BoothListParams,
+  BoothDetailParams,
+} from '@/app/_common/interfaces/booth.interface';
+
+// booth main page all data api function
+export async function getBoothList(params: BoothListParams): Promise<Booth> {
+  const response = await axiosInstance.get('/booth', { params });
+  return response.data;
+}
+
+// booth detail page detail data api function
+export async function getBoothDetail(
+  booth_id: string,
+  params: BoothDetailParams,
+): Promise<BoothDetail> {
+  const response = await axiosInstance.get(`/booth/${booth_id}`, { params });
+  return response.data;
+}
