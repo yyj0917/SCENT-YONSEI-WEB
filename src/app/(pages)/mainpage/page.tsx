5ab7f543c69a56ca3f596fbf017270eeb9c4c5e5
nayeon <yeonamy@yonsei.ac.kr>
2025-05-24 02:38:30 +0900

    feat/ 배경이미지 수정, 카드 애니메이션 추가

--- src/app/(pages)/mainpage/page.tsx
+++ src/app/(pages)/mainpage/page.tsx
@@ -3,26 +3,39 @@ import DayCard from './components/day-card';
 import { DayCardData } from './components/day-card-data';
 import UnderBar from '@/app/_common/components/underbar';
 import Button from './components/button';
+import { InView } from '@/app/_core/components/in-view';
+import { Children } from 'react';
 
 export default function MainPage() {
   return (
     <div className='mainpage-background min-h-screen w-full'>
+      
       <TopBar title='' />
 
       <main className='flex flex-col'>
-        <div className='w-full h-[530px] mt-[121px] bg-[#44BDF5] bg-[url("/img/mainpage/mainpage-logo.png")]  bg-cover bg-center bg-no-repeat'></div>
 
-        <section className='w-full flex flex-col items-center gap-[48px] mt-10 mx-auto'>
+        <section className='w-full flex flex-col items-center gap-[48px] mt-[800px] mx-auto'>
           {DayCardData.map((card, idx) => (
-            <DayCard key={idx} image={card.image} texts={card.texts} />
+            <InView 
+              key={idx}
+              as="div"
+              variants={{
+                hidden: { opacity: 0, y: 100, filter: 'blur(4px)' },
+                visible: { opacity: 1, y: 0, filter: 'blur(0px)' },
+              }}
+              viewOptions={{ margin: '0px 0px -100px 0px' }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+            <DayCard image={card.image} texts={card.texts} />
+            </InView>
           ))}
         </section>
-
+        
         <div className='flex flex-col items-center gap-[22px] mt-[57px] mb-[87px]'>
           <Button href='/booth'>부스 정보 보러가기</Button>
-
-          <Button href='/booth'>공연 정보 보러가기</Button>
+          <Button href='/festival'>공연 정보 보러가기</Button>
         </div>
+
       </main>
       <footer className='flex items-center justify-center'>
         <UnderBar />
