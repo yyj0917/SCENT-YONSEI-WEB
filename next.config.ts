b6816bae23094a67013e8bad7a51929f1a123f6f
yyj0917 <yyj0917@yonsei.ac.kr>
2025-05-24 17:26:22 +0900

    fix: next.config.ts domains -> remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['scent-photo-bucket.s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        hostname: 'scent-photo-bucket.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
   },
   webpack(config) {
     config.module.rules.push({
